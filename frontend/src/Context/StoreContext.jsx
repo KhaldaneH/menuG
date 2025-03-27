02033f4dc73ec2d821db93c9c18ab2b91cab34e1
KhaldaneH <122120678+KhaldaneH@users.noreply.github.com>
2025-03-27 19:26:39 +0100

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://menug.onrender.com/"
+    const url = "https://menug.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
